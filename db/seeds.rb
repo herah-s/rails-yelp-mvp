fee8032df5609a0496d8dc0b3b02d58295d58aec
Rina Iwamine <leddihome@gmail.com>
2023-11-08 15:52:50 +0900

    added all user stories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts 'Cleaning db...'
+Restaurant.destroy_all
+
+5.times do
+  puts 'Creating restaurants...'
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: CATEGORIES.sample
+  )
+end
+
+puts 'Finished!'
