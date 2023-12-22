3ed504c954988ab5883d2e0a9cce28908ef6ab72
Rina Iwamine <leddihome@gmail.com>
2023-12-22 18:12:10 +0900

    changed migration

--- db/migrate/20231108023240_create_reviews.rb
+++ db/migrate/20231108023240_create_reviews.rb
@@ -1,11 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
-  def change
-    create_table :reviews do |t|
-      t.text :content
-      t.integer :rating
-      t.references :restaurant, null: false, foreign_key: true
+  # def change
+  #   create_table :reviews do |t|
+  #     t.text :content
+  #     t.integer :rating
+  #     t.references :restaurant, null: false, foreign_key: true
 
-      t.timestamps
-    end
-  end
+  #     t.timestamps
+  #   end
+  # end
 end
